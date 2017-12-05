1b3730585db502e6a22146b6a92843877cd0ed16
hensm <m+github@matt.tf>
2017-12-05 07:52:23 +0000

    Don't trigger on non-keyboard inputs

--- content.js
+++ content.js
@@ -1,5 +1,18 @@
 "use strict";
 
+/**
+ * The last keypress in a focused element is recorded and
+ * checked against the target of an input event to verify a
+ * genuine keyboard action.
+ */
+let pending_input = null;
+
+window.addEventListener("keypress", ev => {
+    const focused_el = document.querySelector(":focus");
+    pending_input = focused_el || null;
+});
+
+
 // Whether listeners have been set
 let is_listening = false;
 
@@ -22,15 +35,35 @@ function on_mouse_event (ev) {
     });
 }
 
-window.addEventListener("input", ev => {
-    browser.runtime.sendMessage({
-        subject: "hide_cursor"
-    });
+/**
+ * Checks input/clipboard events against a recorded keypress
+ * before hiding the cursor.
+ */
+function on_input (ev) {
+    if (pending_input && ev.target === pending_input) {
+        pending_input = null;
 
-    if (!is_listening) {
-        for (const type of event_types) {
-            window.addEventListener(type, on_mouse_event);
+        browser.runtime.sendMessage({
+            subject: "hide_cursor"
+        });
+
+        if (!is_listening) {
+            // Add event listeners to unhide cursor
+            for (const type of event_types) {
+                window.addEventListener(type, on_mouse_event);
+            }
+            is_listening = true;
         }
-        is_listening = true;
     }
+}
+
+window.addEventListener("input", on_input);
+window.addEventListener("paste", on_input);
+window.addEventListener("cut", on_input);
+
+// Show cursor on tab change
+window.addEventListener("blur", ev => {
+    browser.runtime.sendMessage({
+        subject: "show_cursor"
+    });
 });
